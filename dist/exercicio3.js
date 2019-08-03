0fba8564d0cc53175417492cebb3050316dabffa
igor martuci <igormartuci99@gmail.com>
2019-08-02 22:29:39 -0300

    igor e thiago ex3 aula2 pw3

--- dist/exercicio3.js
+++ dist/exercicio3.js
@@ -1,9 +1,10 @@
 var nt1 = 10;
 var nt2 = 5;
 var nt3 = 7;
-var media = function (n1, n2, n3) {
-    return (n1 + n2 + n3) / 3;
-};
+/* let media = function(n1:number, n2:number, n3:number) : number {
+    return (n1 + n2 + n3) / 3
+} */
+var media = function (n1, n2, n3) { return (n1 + n2 + n3) / 3; };
 /* if (media(5,6,7) < 5) {
     console.log("reprovado")
 } else {
--- exercicio3.ts
+++ exercicio3.ts
@@ -2,10 +2,11 @@ let nt1: number = 10
 let nt2: number = 5
 let nt3: number = 7
 
-let media = function(n1:number, n2:number, n3:number) : number {
+/* let media = function(n1:number, n2:number, n3:number) : number {
     return (n1 + n2 + n3) / 3
-}
+} */
 
+let media = (n1:number, n2:number, n3:number) => (n1 + n2 + n3) / 3
 
 /* if (media(5,6,7) < 5) {
     console.log("reprovado")
